14eea594b8539731b85b78eb48e6e812d4028a4a
Rajan <rajanj448@student.sfit.ac.in>
2024-11-05 11:23:24 +0530

    solved image rendering

---
+++ backend/src/controllers/product.ts
@@ -0,0 +1,263 @@
+import { Request } from "express";
+import { TryCatch } from "../middlewares/error.js";
+import { Product } from "../models/product.js";
+import { Review } from "../models/review.js";
+import { User } from "../models/user.js";
+import {
+  BaseQuery,
+  NewProductRequestBody,
+  SearchRequestQuery,
+} from "../types/types.js";
+import {
+  deleteFromCloudinary,
+  findAverageRatings,
+  uploadToCloudinary,
+} from "../utils/features.js";
+import ErrorHandler from "../utils/utility-class.js";
+
+// Get Latest Products
+export const getlatestProducts = TryCatch(async (req, res, next) => {
+  const products = await Product.find({}).sort({ createdAt: -1 }).limit(5);
+
+  return res.status(200).json({
+    success: true,
+    products,
+  });
+});
+
+// Get All Categories
+export const getAllCategories = TryCatch(async (req, res, next) => {
+  const categories = await Product.distinct("category");
+
+  return res.status(200).json({
+    success: true,
+    categories,
+  });
+});
+
+// Get Admin Products
+export const getAdminProducts = TryCatch(async (req, res, next) => {
+  const products = await Product.find({});
+
+  return res.status(200).json({
+    success: true,
+    products,
+  });
+});
+
+// Get Single Product by ID
+export const getSingleProduct = TryCatch(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) return next(new ErrorHandler("Product Not Found", 404));
+
+  return res.status(200).json({
+    success: true,
+    product,
+  });
+});
+
+// Create New Product
+export const newProduct = TryCatch(
+  async (req: Request<{}, {}, NewProductRequestBody>, res, next) => {
+    const { name, price, stock, category, description } = req.body;
+    const photos = req.files as Express.Multer.File[] | undefined;
+
+    if (!photos) return next(new ErrorHandler("Please add Photo", 400));
+    if (photos.length < 1) return next(new ErrorHandler("Please add at least one Photo", 400));
+    if (photos.length > 5) return next(new ErrorHandler("You can only upload 5 Photos", 400));
+    if (!name || !price || !stock || !category || !description)
+      return next(new ErrorHandler("Please enter All Fields", 400));
+
+    const photosURL = await uploadToCloudinary(photos);
+
+    await Product.create({
+      name,
+      price,
+      description,
+      stock,
+      category: category.toLowerCase(),
+      photos: photosURL,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "Product Created Successfully",
+    });
+  }
+);
+
+// Update Product by ID
+export const updateProduct = TryCatch(async (req, res, next) => {
+  const { id } = req.params;
+  const { name, price, stock, category, description } = req.body;
+  const photos = req.files as Express.Multer.File[] | undefined;
+
+  const product = await Product.findById(id);
+
+  if (!product) return next(new ErrorHandler("Product Not Found", 404));
+
+  if (photos && photos.length > 0) {
+    const photosURL = await uploadToCloudinary(photos);
+    const ids = product.photos.map((photo) => photo.public_id);
+    await deleteFromCloudinary(ids);
+    product.photos = photosURL;
+  }
+
+  if (name) product.name = name;
+  if (price) product.price = price;
+  if (stock) product.stock = stock;
+  if (category) product.category = category;
+  if (description) product.description = description;
+
+  await product.save();
+
+  return res.status(200).json({
+    success: true,
+    message: "Product Updated Successfully",
+  });
+});
+
+// Delete Product by ID
+export const deleteProduct = TryCatch(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) return next(new ErrorHandler("Product Not Found", 404));
+
+  const ids = product.photos.map((photo) => photo.public_id);
+  await deleteFromCloudinary(ids);
+
+  await product.deleteOne();
+
+  return res.status(200).json({
+    success: true,
+    message: "Product Deleted Successfully",
+  });
+});
+
+// Get All Products with Filters
+export const getAllProducts = TryCatch(
+  async (req: Request<{}, {}, {}, SearchRequestQuery>, res, next) => {
+    const { search, sort, category, price } = req.query;
+    const page = Number(req.query.page) || 1;
+
+    const limit = Number(process.env.PRODUCT_PER_PAGE) || 8;
+    const skip = (page - 1) * limit;
+
+    const baseQuery: BaseQuery = {};
+
+    if (search)
+      baseQuery.name = {
+        $regex: search,
+        $options: "i",
+      };
+
+    if (price)
+      baseQuery.price = {
+        $lte: Number(price),
+      };
+
+    if (category) baseQuery.category = category;
+
+    const productsPromise = Product.find(baseQuery)
+      .sort(sort && { price: sort === "asc" ? 1 : -1 })
+      .limit(limit)
+      .skip(skip);
+
+    const [products, filteredOnlyProduct] = await Promise.all([
+      productsPromise,
+      Product.find(baseQuery),
+    ]);
+
+    const totalPage = Math.ceil(filteredOnlyProduct.length / limit);
+
+    return res.status(200).json({
+      success: true,
+      products,
+      totalPage,
+    });
+  }
+);
+
+// Get All Reviews of a Product by ID
+export const allReviewsOfProduct = TryCatch(async (req, res, next) => {
+  const reviews = await Review.find({
+    product: req.params.id,
+  })
+    .populate("user", "name photo")
+    .sort({ updatedAt: -1 });
+
+  return res.status(200).json({
+    success: true,
+    reviews,
+  });
+});
+
+// Add or Update Review for a Product
+export const newReview = TryCatch(async (req, res, next) => {
+  const user = await User.findById(req.query.id);
+  if (!user) return next(new ErrorHandler("Not Logged In", 404));
+
+  const product = await Product.findById(req.params.id);
+  if (!product) return next(new ErrorHandler("Product Not Found", 404));
+
+  const { comment, rating } = req.body;
+
+  const alreadyReviewed = await Review.findOne({
+    user: user._id,
+    product: product._id,
+  });
+
+  if (alreadyReviewed) {
+    alreadyReviewed.comment = comment;
+    alreadyReviewed.rating = rating;
+    await alreadyReviewed.save();
+  } else {
+    await Review.create({
+      comment,
+      rating,
+      user: user._id,
+      product: product._id,
+    });
+  }
+
+  const { ratings, numOfReviews } = await findAverageRatings(product._id);
+
+  product.ratings = ratings;
+  product.numOfReviews = numOfReviews;
+
+  await product.save();
+
+  return res.status(alreadyReviewed ? 200 : 201).json({
+    success: true,
+    message: alreadyReviewed ? "Review Updated" : "Review Added",
+  });
+});
+
+// Delete Review by ID
+export const deleteReview = TryCatch(async (req, res, next) => {
+  const user = await User.findById(req.query.id);
+  if (!user) return next(new ErrorHandler("Not Logged In", 404));
+
+  const review = await Review.findById(req.params.id);
+  if (!review) return next(new ErrorHandler("Review Not Found", 404));
+
+  const isAuthenticUser = review.user.toString() === user._id.toString();
+  if (!isAuthenticUser) return next(new ErrorHandler("Not Authorized", 401));
+
+  await review.deleteOne();
+
+  const product = await Product.findById(review.product);
+  if (!product) return next(new ErrorHandler("Product Not Found", 404));
+
+  const { ratings, numOfReviews } = await findAverageRatings(product._id);
+
+  product.ratings = ratings;
+  product.numOfReviews = numOfReviews;
+
+  await product.save();
+
+  return res.status(200).json({
+    success: true,
+    message: "Review Deleted",
+  });
+});
